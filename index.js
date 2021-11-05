13729ec61638b25548dd8b1e90256d6ff3a9699b
iraytee-code <adeniranhope21@gmail.com>
2021-11-05 11:11:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+"use strict";
+//header feature
+const navTag = document.createElement("nav");
+navTag.setAttribute("id", "title");
+navTag.classList.add("title");
+
+navTag.innerHTML = `<h1 class="header_text">Simple, traffic-based pricing</h1>
+<p class="paragraph_text">Sign-up for our 30-day trial. No credit card required.</p>`;
+
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+header.append(navTag);
+
+const mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "main_div");
+mainDiv.classList.add("main_div");
+//pricing feature
+mainDiv.innerHTML = `
+<div class="value view_value">
+<p class="view_svalue"><span id="demo"><p>K Page Views</p></span></p> 
+</div>
+<div class="value"><span class="price_value">$16</span> /month</div>
+`;
+
+main.append(mainDiv);
+// slider feature
+
+const valueSlider = document.createElement("div");
+valueSlider.classList.add("slidercontainer");
+
+valueSlider.innerHTML = `<div class="slidecontainer">
+<input type="range" min="1" max="100" value="50" class="slider" id="myRange"></div>`;
+
+main.append(valueSlider);
+
+//billing duration
+const durationDiv = document.createElement("div");
+durationDiv.classList.add("duration_div");
+main.append(durationDiv);
+
+const billDuration = document.createElement("div");
+billDuration.classList.add("bill_duration");
+billDuration.setAttribute("id", "duration");
+
+billDuration.innerHTML = `
+<div class="sub-type">Monthly Billing</div>
+<label class="switch">
+  <input type="checkbox">
+  <span class="slider_ball round"></span>
+</label>
+<div class="sub-type">Yearly Billing</div>
+`;
+
+durationDiv.append(billDuration);
+
+//start my trial info
+
+const faqDetails = document.createElement("div");
+faqDetails.classList.add("details");
+
+faqDetails.innerHTML = `
+<div class="sub_details"><ul><li class="list-type">
+<img src="./images/icon-check.svg" alt="" /> Unlimited websites<li class="list-type">
+<img src="./images/icon-check.svg" alt="" /> 100% data
+ownership</li><li class="list-type">
+<img src="./images/icon-check.svg" alt="" /> Email reports</li></ul></div>
+<div class="sub_trial"><button class="start_button">Start my trial</button></div>
+`;
+
+main.append(faqDetails);
+
+//slider value output
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
